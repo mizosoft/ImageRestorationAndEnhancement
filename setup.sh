5c315f8656dd15b8c22ed0df79b9062e562ee332
Moataz Abdelnasser <moataz.nasser20@gmail.com>
2022-07-03 07:55:19 +0200

    Download DeOldify weights in setup.sh

--- setup.sh
+++ setup.sh
@@ -12,15 +12,25 @@ fi
 if [[ ! -d 'weights' ]]
 then
   mkdir 'weights'
+
+  while read -r line; do
+    echo "Downloading $line"
+    wget -nc "$line" -P weights
+  done < gpen_urls.txt
+
+  for i in $(find 'weights' -type f)
+  do
+      mv "$i" "$(echo "$i" | cut -d? -f1)"
+  done
 fi
 
-while read -r line; do
-  echo "Downloading $line"
-  wget -nc "$line" -P weights
-done < gpen_urls.txt
+if [[ ! -d 'models' ]]
+then
+#  wget -nc https://data.deepai.org/deoldify/ColorizeArtistic_gen.pth -P models
+  wget -nc https://www.dropbox.com/s/usf7uifrctqw9rl/ColorizeStable_gen.pth?dl=0 -P models
 
-# wget is retarded
-for i in $(find 'weights' -type f)
-do
-    mv "$i" "$(echo "$i" | cut -d? -f1)"
-done
+  for i in $(find 'weights' -type f)
+  do
+      mv "$i" "$(echo "$i" | cut -d? -f1)"
+  done
+fi
